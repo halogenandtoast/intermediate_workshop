17bbe254e9acfea6a18760cecde1a5a59c1951ce
Matthew Mongeau <halogenandtoast@gmail.com>
2012-04-02 10:00:56 -0400

    Initial rails app + clearance setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '4ce51d54198a7c1c583131b93e5b2876fcf48570e095f047992fd0d8cccd414114fd845e42170e9e567b233436d76a3e5075c18481488ba84b6f7e27cb0232c8'
