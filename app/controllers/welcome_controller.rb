86e4388ef1941000455eff3db9bb25b9d23c68f1
Matthew Mongeau <halogenandtoast@gmail.com>
2012-04-02 11:05:53 -0400

    Added dashboard and redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,11 @@
 class WelcomeController < ApplicationController
+  before_filter :redirect_to_dashboard_when_signed_in
+  def index
+    @user = User.new
+  end
 
+  private
+  def redirect_to_dashboard_when_signed_in
+    redirect_to dashboard_path if signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Shouter::Application.routes.draw do
   root to: "welcome#index"
+  resource :dashboard, only: [:show]
 end
